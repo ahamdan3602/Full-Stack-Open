c51d2e362b4b412643e559d009fae7be99ad2550
Abdul Al-rahman Mohammed Hamdan <ahamdan4@uwo.ca>
2025-05-28 05:01:20 -0400

    .

--- part1/introdemo/src/App.jsx
+++ part1/introdemo/src/App.jsx
@@ -1,32 +1,16 @@
 import { useState } from "react";
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
-  console.log("rendering with counter value", counter);
-
-  const increaseByOne = () => {
-    console.log("increasing value before", counter);
-    setCounter(counter + 1);
-  };
-
-  const setToZero = () => {
-    console.log("resetting to zero, value before", counter);
-    setCounter(0);
-  };
-
-  const decreaseByOne = () => {
-    console.log("decreasing, value before", counter);
-    setCounter(counter - 1);
-  };
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
 
   return (
     <div>
-      <div>{counter}</div>
-      <button onClick={increaseByOne}>plus</button>
-      <button onClick={setToZero}>zero</button>
-      <button onClick={decreaseByOne}>minus</button>
+      {left}
+      <button onClick={() => setLeft(left + 1)}>left</button>
+      <button onClick={() => setRight(right + 1)}>right</button>
+      {right}
     </div>
   );
 };
-
 export default App;
