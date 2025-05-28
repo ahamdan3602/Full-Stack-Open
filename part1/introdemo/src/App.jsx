ab7fb309cb09c89f36f6a9d59d63b4e110c6ff1b
Abdul Al-rahman Mohammed Hamdan <ahamdan4@uwo.ca>
2025-05-28 18:28:10 -0400

    conditional rendering

--- part1/introdemo/src/App.jsx
+++ part1/introdemo/src/App.jsx
@@ -1,15 +1,45 @@
 import { useState } from "react";
 
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return <div>the app is used by pressing the byttons</div>;
+  }
+  return <div>button press history: {props.allClicks.join(" ")}</div>;
+};
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
 const App = () => {
   const [left, setLeft] = useState(0);
   const [right, setRight] = useState(0);
+  const [allClicks, setAll] = useState([]);
+  const [total, setTotal] = useState(0);
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat("L"));
+    console.log("left before", left);
+    const updatedLeft = left + 1;
+    setLeft(updatedLeft);
+    console.log("left after", updatedLeft);
+    setTotal(updatedLeft + right);
+  };
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat("R"));
+    console.log("right before", right);
+    const updatedRight = right + 1;
+    setRight(updatedRight);
+    console.log("right after", updatedRight);
+    setTotal(left + updatedRight);
+  };
 
   return (
     <div>
       {left}
-      <button onClick={() => setLeft(left + 1)}>left</button>
-      <button onClick={() => setRight(right + 1)}>right</button>
+      <Button onClick={handleLeftClick} text="left" />
+      <Button onClick={handleRightClick} text="right" />
       {right}
+      <History allClicks={allClicks} />
+      <p>Total: {total}</p>
     </div>
   );
 };
