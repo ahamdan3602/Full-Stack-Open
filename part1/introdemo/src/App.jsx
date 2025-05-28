b0f03b8e335133cbbb0a980060bb5b67509170ed
Abdul Al-rahman Mohammed Hamdan <ahamdan4@uwo.ca>
2025-05-28 04:11:20 -0400

    part 1

--- part1/introdemo/src/App.jsx
+++ part1/introdemo/src/App.jsx
@@ -1,40 +1,32 @@
-/* eslint-disable no-unused-vars */
-// const Hello = () => {
-//   return (
-//     <div>
-//       <p>Hello World</p>
-//     </div>
-//   );
-// };
-
-// the file imports useState function
 import { useState } from "react";
-//Props
 
-const Hello = ({ name, age }) => {
-  const bornYear = () => new Date().getFullYear() - age;
+const App = () => {
+  const [counter, setCounter] = useState(0);
+  console.log("rendering with counter value", counter);
+
+  const increaseByOne = () => {
+    console.log("increasing value before", counter);
+    setCounter(counter + 1);
+  };
+
+  const setToZero = () => {
+    console.log("resetting to zero, value before", counter);
+    setCounter(0);
+  };
+
+  const decreaseByOne = () => {
+    console.log("decreasing, value before", counter);
+    setCounter(counter - 1);
+  };
+
   return (
     <div>
-      <p>
-        Hello {name}, you are {age} years old{" "}
-      </p>
-      <p>So you were probably born in {bornYear()}</p>
+      <div>{counter}</div>
+      <button onClick={increaseByOne}>plus</button>
+      <button onClick={setToZero}>zero</button>
+      <button onClick={decreaseByOne}>minus</button>
     </div>
   );
 };
 
-const App = () => {
-  /* The function adds state to the component and
-   * renders it initialzied with the value zero.
-   *
-   */
-  const [counter, setCounter] = useState(0);
-
-  setTimeout(() => setCounter(counter + 1), 1000);
-  return <div>{counter}</div>;
-};
-
-// Technically defined as a function: () => {}.... Which is also assigned to constant variable called App
-
-export default App; // Bottom line should never be removed
-// Without the export the whole app breaks down
+export default App;
--- part1/introdemo/src/main.jsx
+++ part1/introdemo/src/main.jsx
@@ -4,17 +4,15 @@ import App from "./App.jsx";
 
 let counter = 1;
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root")).render(<App />);
 
 const refresh = () => {
   root.render(<App counter={counter} />);
 };
 refresh();
-// counter += 1;
-// refresh();
-// counter += 1;
-// refresh();
-// We can do setInterval which essentially allows us to increment the counter by a set time
+
+
+
 
 setInterval(() => {
   refresh();
