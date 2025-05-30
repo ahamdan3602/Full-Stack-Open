4211aa77e91cc46defd04fecf780254dfec7ad1b
Abdul Al-rahman Mohammed Hamdan <ahamdan4@uwo.ca>
2025-05-30 02:56:05 -0400

    added incrementer

--- part1/introdemo/src/App.jsx
+++ part1/introdemo/src/App.jsx
@@ -7,67 +7,22 @@ const History = (props) => {
   return <div>button press history: {props.allClicks.join(" ")}</div>;
 };
 
-const Button = (props) => {
-  console.log("props value is ", props);
-  const { onClick, text } = props;
-  return <button onClick={onClick}>{text}</button>;
-};
-
+const Button = (props) => <button onClick={props.onClick}>{props.text}</button>;
+const Display = (props) => <div>{props.value}</div>;
 const App = () => {
-  const [left, setLeft] = useState(0);
-  const [right, setRight] = useState(0);
-  const [allClicks, setAll] = useState([]);
-  const [total, setTotal] = useState(0);
   const [value, setValue] = useState(10);
 
-  const handleClick = () => {
-    console.log("clicked the button");
-    setValue(0);
-  };
-
   const setToValue = (newValue) => {
-    console.log("value now", newValue); // print the new value to console
+    console.log("value now", newValue);
     setValue(newValue);
-  };
-
-  const hello = (who) => () => {
-    console.log("hello", who);
-  };
-
-  const handleLeftClick = () => {
-    setAll(allClicks.concat("L"));
-    console.log("left before", left);
-    const updatedLeft = left + 1;
-    setLeft(updatedLeft);
-    console.log("left after", updatedLeft);
-    setTotal(updatedLeft + right);
-  };
-
-  const handleRightClick = () => {
-    setAll(allClicks.concat("R"));
-    console.log("right before", right);
-    const updatedRight = right + 1;
-    setRight(updatedRight);
-    console.log("right after", updatedRight);
-    setTotal(left + updatedRight);
   };
 
   return (
     <div>
-      {left}
-      <Button onClick={handleLeftClick} text="left" />
       {value}
-      <button onClick={hello("world")}>button</button>
-      <button onClick={hello("react")}>button</button>
-      <button onClick={hello("function")}>button</button>
-      <button onClick={() => setToValue(1000)}>thousand</button>
-      <button onClick={() => setToValue(0)}>reset</button>
-      <button onClick={() => setToValue(value + 1)}>increment</button>
-
-      <Button onClick={handleRightClick} text="right" />
-      {right}
-      <History allClicks={allClicks} />
-      <p>Total: {total}</p>
+      <Button onClick={() => setToValue(1000)} text="thousand" />
+      <Button onClick={() => setToValue(0)} text="reset" />
+      <Button onClick={() => setToValue(value + 1)} text="increment" />
     </div>
   );
 };
