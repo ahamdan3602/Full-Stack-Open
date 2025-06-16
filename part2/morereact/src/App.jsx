8ab3b638db7d2f7851aec5bdde7ea6e713a73ffd
Abdul Al-rahman Mohammed Hamdan <ahamdan4@uwo.ca>
2025-06-15 22:10:48 -0400

    finished part b forms

--- part2/morereact/src/App.jsx
+++ part2/morereact/src/App.jsx
@@ -3,28 +3,49 @@ import { useState } from "react";
 import "./App.css";
 import Note from "./components/Note.jsx";
 
-const App = ({ notes }) => {
-  // map will always create a new array of whic have been created from elements of the original array by mapping
-  // -> using the function given as a paramete to the map method
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNotes] = useState("a new note ...");
+  const [showAll, setShowAll] = useState(true);
 
-  /**
-   * Full form would be
-   * const res = notes.map((note) => {
-   *    return note.id
-   * })
-   */
-  const res = notes.map((note) => note);
-  console.log(res);
-  // returns an array containng the contents of the notes
+  // The result variable will be set to the value val1 if the condition is true. if condition is fasle, the reulst vairable will be set to the value of val 2
+  /*** const result = condition ? val1 : val2 */
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important);
+
+  const addNote = (event) => {
+    event.preventDefault();
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: String(notes.length + 1),
+    };
+    setNotes(notes.concat(noteObject));
+    setNewNotes("");
+    console.log("button clicked", event.target);
+  };
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value);
+    setNewNotes(event.target.value);
+  };
 
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? "important" : "all"}
+        </button>
+      </div>
       <ul>
-        {notes.map((note) => (
+        {notesToShow.map((note) => (
           <Note key={note.id} note={note} />
         ))}
       </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
     </div>
   );
 };
